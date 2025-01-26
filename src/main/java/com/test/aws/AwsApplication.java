13bfd6ae8e4f0971ff804defa347af180dcc451a
sangkyounge <shin8877123@naver.com>
2025-01-26 14:13:17 +0900

    remove

---
+++ src/main/java/com/test/aws/AwsApplication.java
@@ -0,0 +1,15 @@
+package com.test.aws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+
+@SpringBootApplication
+public class AwsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AwsApplication.class, args);
+    }
+
+}
